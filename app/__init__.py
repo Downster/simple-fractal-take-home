7fbe1f9799218974f1ef68bca3e9700483e3d2e4
Brendan Downing <b.downing.code@gmail.com>
2022-08-31 22:38:51 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 #Route that returns the coding and communication percentile of a candidate and information for a graph
 @app.route('/percentile/<int:candidate_id>')
 def percentile(candidate_id):
+    """Takes in a candidate_id and returns the scores of other similar candidates, and the percentile of the provided candidate in a dictionary
+        scores can be accessed by keying into 'scores'
+        percentile can be accessed by keying into 'percentile'
+        the percentile dictionary has two keys 'coding_percentile' and 'communication_percentile'
+        which correspond to the candidates coding percentile and communication percentile, respectively
+    """
     #Check and see if a valid candidate id has been passed through
     #react-hook-form validations should take care of this
     #but we will define it ourselves just to be sure
